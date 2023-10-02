79b1baf3faa1e08046a1956000882d779524025a
Jack Hollingsworth <jackhhollings@gmail.com>
2023-10-02 15:16:07 -0500

    add cs1test3.m

---
+++ cs1test3.m
@@ -0,0 +1,6 @@
+load("COVIDbyCounty.mat");
+load("ogData.mat"); %load the data yielded by casestudy1test.m - to declutter iterative files
+%this file will be used to build average centroids and complete a call to
+%kmeans using these 9 average centroids as starting points for kmeans
+%using cs1test2.m as a template for the code here...
+
