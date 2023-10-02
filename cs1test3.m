2ef2749b2a28ac2b24afc6c9f1891aa037c93300
Jack Hollingsworth <jackhhollings@gmail.com>
2023-10-02 17:47:45 -0500

    upddates to cs1test2.m - added modular filtering code

--- cs1test3.m
+++ cs1test3.m
@@ -4,3 +4,6 @@ load("ogData.mat"); %load the data yielded by casestudy1test.m - to declutter it
 %kmeans using these 9 average centroids as starting points for kmeans
 %using cs1test2.m as a template for the code here...
 
+rng(2,"v4"); %set rng to v4 protocol with a seed of 2 - this yielded the highest average silhouette during testing
+options = statset('UseParallel',true,'MaxFunEvals',100,'DerivStep',100); %configure furhter options for kmeans call
+
