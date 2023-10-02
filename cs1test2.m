e9024efad183e58c2ec65e34a85617e2937abfb4
Jack Hollingsworth <jackhhollings@gmail.com>
2023-10-02 15:11:00 -0500

    update cs1test2.m - add code to generate an average cluster of a division

--- cs1test2.m
+++ cs1test2.m
@@ -9,9 +9,12 @@ load("ogData.mat"); %load the data yielded by casestudy1test.m - to declutter it
 %for each division. Then run kmeans again using these centroids as starting
 %points for a call to kmeans using the entire training set, to yield final,
 %more accurate centroids that consider the entire set of data. 
-rng(2,"v4");
-options = statset('UseParallel',true,'MaxFunEvals',100,'DerivStep',100);
-[testIDX, testCentroids] = kmeans(division1training,6,'replicates',20,'distance','sqeuclidean','start','plus','options',options);
-division1silhouettes = silhouette(division1training,testIDX);
-avgD1silhouette = mean(division1silhouettes);
+
+rng(2,"v4"); %set rng to v4 protocol with a seed of 2 - this yielded the highest average silhouette during testing
+options = statset('UseParallel',true,'MaxFunEvals',100,'DerivStep',100); %configure furhter options for kmeans call
+[testIDX, testCentroids] = kmeans(division1training,6,'replicates',20,'distance','sqeuclidean','start','plus','options',options); %call kmeans on the first division
+division1silhouettes = silhouette(division1training,testIDX); %calculate silhouette values for the centroids generated
+avgD1silhouette = mean(division1silhouettes); %computer average value of the division 1 silhouettes
 disp(avgD1silhouette);
+
+[d1avgIDX, d1avgCentroid] = kmeans(testCentroids,1); %generate an average of the six centroids for the first division, to be used as a starting centroid in the final kmeans call
